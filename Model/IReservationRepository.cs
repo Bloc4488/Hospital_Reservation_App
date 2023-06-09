70be62a72bdfe834d34e5e65d2b2fb3650519ff5
iliubin7 <257798@student.pwr.edu.pl>
2023-06-10 01:31:33 +0300

    Create reservatino repository

---
+++ Model/IReservationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Reservation_App.Model
+{
+    interface IReservationRepository
+    {
+       void AddRes(int reservation_id, int doctor_id, DateTime res);
+    }
+}
--- ViewModel/AddReservationViewModel.cs
+++ ViewModel/AddReservationViewModel.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Hospital_Reservation_App.ViewModel
 {
     public class AddReservationViewModel : ViewModelBase
     {
+        private bool _isVisible;
+
+        public bool IsVisible
+        {
+            get { return _isVisible; }
+            set
+            {
+                if (_isVisible != value)
+                {
+                    _isVisible = value;
+                    OnPropertyChanged(nameof(IsVisible));
+                }
+            }
+        }
+
+        public ICommand AddReservationCommand { get; }
+        public AddReservationViewModel()
+        {
+            AddReservationCommand = new ViewModelCommand(ExecuteAddReservationCommand, CanExecuteAddReservationCommand);
+        }
+        private bool CanExecuteAddReservationCommand(object obj)
+        {
+            bool validAdd = true;
+            return validAdd;
+        }
+        private void ExecuteAddReservationCommand(object obj)
+        {
+
+        }
     }
 }
